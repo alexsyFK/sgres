41dab007a1bc756777ef2135ea01917e091cd74c
root <root@dfc2eabdf236>
2017-04-02 18:55:13 +0000

    updating 04:02pm 04/02/207 alexsyfk

--- sql/script.sql
+++ sql/script.sql
@@ -1,5 +1,10 @@
 /*insert into db_contas(nome)values('bruno');select nome from db_sgres.db_contas order by nome desc;*/
 --delete from db_contas where nome='bruno';
+insert into db_info(nome,idade) values('ana patricia',23); 
+
+delete from db_info where idade is null;
+
+
 /*
 
 <!--
